86dbe2ded5e5cd9ca069b1d7005b9c39a874aa7f
michaelnji <73862378+michaelnji@users.noreply.github.com>
2023-08-22 00:59:13 +0200

    style: :sparkles: Built hero section of landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,23 @@
-<script>
+<script lang="ts">
+	import Navbar from '$lib/components/navbar.svelte';
 	import './styles.css';
+	import '@fontsource/aladin';
+	import '@fontsource/work-sans';
+	import '@fontsource/work-sans/400.css';
+	import '@fontsource/work-sans/500.css';
+	import '@fontsource/work-sans/600.css';
+	import '@fontsource/work-sans/700.css';
+	import '@fontsource/work-sans/800.css';
+	import '@fontsource/work-sans/900.css';
+	// import '@fontsource/didact-gothic/400.css';
+	import '@fontsource/gochi-hand';
 </script>
 
-<div class="app">
-	<main>
+<div class="app bg-base-300 !h-screen w-screen overflow-x-hidden font-body text-md">
+	<header>
+		<Navbar />
+	</header>
+	<main class="pt-24 mx-6 !h-full">
 		<slot />
 	</main>
 </div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import { formatNum, formatNumCurrency } from '../lib/scripts/numberFunctions';
+	const num: string = formatNumCurrency(2000.0, 'usd');
+</script>
+
+<section class="sm:mt-6 mt-2">
+	<!-- hero section -->
+	<div class="w-full min-h-full place-content-center grid grid-cols-1 place-items-center my-auto">
+		<!-- text -->
+		<h1 class="font-extrabold text-6xl md:text-7xl lg:text-8xl max-w-7xl text-center font-head">
+			Your personal <span class="text-primary">sauce</span> for juicy Waifu pictures
+		</h1>
+		<p class="mt-3 text-center text-xl max-w-3xl opacity-90">
+			Looking for a source of pretty waifu pics? check out our collection of over <b
+				class="underline underline-offset-1 text-primary"
+				>{formatNum(4900).split('.')[0]}+ saucy images</b
+			>
+			for the pleasure of your 👀
+		</p>
+		<div class="flex gap-2 mt-8 items-center justify-center flex-wrap !mx-auto">
+			<a href="/#all" class="btn btn-wide btn-primary">View images</a>
+			<a href="https://github.com/michaelnji/kawaiii" class="btn btn-wide btn-primary btn-outline"
+				>github</a
+			>
+		</div>
+	</div>
+</section>
+<div class="mx-auto md:mt-28 mt-16 flex items-center justify-center">
+	<div class="animate-bounce duration-1000">
+		<svg
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			class="w-[60px] h-[60px] md:w-[100px] md:h-[100px]"
+			><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+				id="SVGRepo_tracerCarrier"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/><g id="SVGRepo_iconCarrier">
+				<path
+					d="M12 20L18 14M12 20L6 14M12 20L12 9.5M12 4V6.5"
+					stroke="#f472b6"
+					stroke-width="1.5"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/>
+			</g></svg
+		>
+	</div>
+</div>
