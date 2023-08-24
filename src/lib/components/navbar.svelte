8741c7cf08ba7355e959398f45867ac1f3168795
michaelnji <73862378+michaelnji@users.noreply.github.com>
2023-08-24 15:22:18 +0200

    feat: :sparkles: Display all tags on navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import { page } from '$app/stores';
 	import Github from './icons/github.svelte';
 	import i from '$lib/images/LOGO.png';
+	import { getTagsClient } from '$lib/scripts/api/data/images';
+	import { isArray } from 'mathjs';
 	let currentString: string;
 	$: currentString = $page.url.pathname;
+	let isLoading: boolean = false;
+	let tags: any = [];
+	const getNavTags = async () => {
+		if (tags.length == 0) {
+			isLoading = true;
+			const tagData = await getTagsClient();
+			tags = tagData;
+			isLoading = false;
+		}
+	};
 </script>
 
 <div class="navbar bg-base-100 fixed z-20 top-0 left-0">
 	<div class="navbar-start">
 		<div class="dropdown">
-			<span tabindex="-1" class="btn btn-ghost lg:hidden">
+			<button tabindex="-1" class="btn btn-ghost btn-sm mr-2 lg:hidden" on:click={getNavTags}>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					class="h-5 w-5"
@@ -23,7 +36,7 @@
 						d="M4 6h16M4 12h8m-8 6h16"
 					/></svg
 				>
-			</span>
+			</button>
 			<ul
 				tabindex="-1"
 				class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
@@ -38,26 +51,27 @@
 					>
 				</li>
 				<li>
-					<a href="/">Tags</a>
+					<span class="hover:bg-transparent !cursor-default">Tags</span>
 					<ul class="p-2">
-						<li>
-							<a
-								href="/"
-								class="font-medium"
-								class:bg-primary={currentString === '/tags/'}
-								class:bg-opacity-30={currentString === '/tags/'}
-								class:text-primary={currentString === '/tags/'}>waifu</a
-							>
-						</li>
-						<li>
-							<a
-								href="/"
-								class="font-medium"
-								class:bg-primary={currentString === '/tags/'}
-								class:bg-opacity-30={currentString === '/tags/'}
-								class:text-primary={currentString === '/tags/'}>otaku</a
-							>
-						</li>
+						{#if isLoading}
+							<div class="loading-spinner loading loading-sm" />
+						{:else if isArray(tags)}
+							{#each tags as tag}
+								<li in:fade>
+									<a
+										href={`/tags/${tag}`}
+										class="font-medium"
+										class:bg-primary={currentString === `/tags/${tag}`}
+										class:bg-opacity-30={currentString === `/tags/${tag}`}
+										class:text-primary={currentString === `/tags/${tag}`}>{tag}</a
+									>
+								</li>
+							{/each}
+						{:else}
+							<li class="p-2 rounded bg-error text-error bg-opacity-10 font-semibold">
+								error occurred
+							</li>
+						{/if}
 					</ul>
 				</li>
 				<li>
@@ -86,31 +100,37 @@
 					class:text-primary={currentString === '/'}>Home</a
 				>
 			</li>
-			<li tabindex="-1">
-				<details>
-					<summary>Tags</summary>
-					<ul class="p-3 space-y-2">
-						<li>
-							<a
-								href="/"
-								class="font-medium"
-								class:bg-primary={currentString === '/tags/'}
-								class:bg-opacity-30={currentString === '/tags/'}
-								class:text-primary={currentString === '/tags/'}>waifu</a
-							>
-						</li>
-						<li>
-							<a
-								href="/"
-								class="font-medium"
-								class:bg-primary={currentString === '/tags/'}
-								class:bg-opacity-30={currentString === '/tags/'}
-								class:text-primary={currentString === '/tags/'}>otaku</a
-							>
-						</li>
+
+			<div class="dropdown dropdown-bottom">
+				<li class="p-0">
+					<span tabindex="-1" on:click={getNavTags} on:keydown class="font-medium w-full h-full">
+						Tags</span
+					>
+					<ul class="p-3 space-y-2 dropdown-content bg-base-100 !min-w-[13rem] rounded-md">
+						{#if isLoading}
+							<li class="flex justify-center items-center">
+								<div class="loading-spinner loading loading-xs" />
+							</li>
+						{:else if isArray(tags)}
+							{#each tags as tag}
+								<li>
+									<a
+										href={`/tags/${tag}`}
+										class="font-medium capitalize"
+										class:bg-primary={currentString === `/tags/${tag}`}
+										class:bg-opacity-30={currentString === `/tags/${tag}`}
+										class:text-primary={currentString === `/tags/${tag}`}>{tag}</a
+									>
+								</li>
+							{/each}
+						{:else}
+							<li class="p-2 rounded bg-error text-error bg-opacity-10 font-semibold">
+								error occurred
+							</li>
+						{/if}
 					</ul>
-				</details>
-			</li>
+				</li>
+			</div>
 			<li>
 				<a
 					href="/about"
